f4c5eee39a69fd2158e6d4f3b671b8c172d88cb4
mahima-khera-codes <mahima.excel2011@gmail.com>
2024-12-04 01:46:41 +0100

    updated files to align with project goals

---
+++ mail_variables.py
@@ -0,0 +1,239 @@
+variables = [
+    {
+        "name": "#interview_test",
+        "value": None,
+        "recruit_variable":"[interview_test]",
+        "variable_type": "user"
+    },
+    {
+        "name": "#candidate_link",
+        "value": None,
+        "recruit_variable":"[candidate_link]",
+        "variable_type": "user"
+    },
+    {
+        "name": "#date",
+        "value": None,
+        "recruit_variable":"[date]",
+        "variable_type": "user"
+    },
+    {
+        "name": "#interview_date",
+        "value": None,
+        "recruit_variable":"[interview_date]",
+        "variable_type": "user"
+    },
+    {
+        "name": "#test_link",
+        "value": None,
+        "recruit_variable":"[test_link]",
+        "variable_type": "user"
+    },
+    {
+        "name": "#interview_time",
+        "value": None,
+        "recruit_variable":"[interview_time]",
+        "variable_type": "user"
+    },
+    {
+        "name": "#email",
+        "value": None,
+        "recruit_variable":"[email]",
+        "variable_type": "user"
+    },
+    {
+        "name": "#password",
+        "value": None,
+        "recruit_variable":"[password]",
+        "variable_type": "user"
+    },
+    {
+        "name": "#role",
+        "value": None,
+        "recruit_variable":"[role]",
+        "variable_type": "user"
+    },
+    {
+        "name": "#website_url",
+        "value": None,
+        "recruit_variable":"[website_url]",
+        "variable_type": "user"
+    },
+    {
+        "name": "#round_name",
+        "value": None,
+        "recruit_variable":"[round_name]",
+        "variable_type": "user"
+    },
+    {
+        "name": "#time",
+        "value": None,
+        "recruit_variable":"[time]",
+        "variable_type": "user"
+    },
+    {
+        "name": "#job_profile",
+        "value": None,
+        "recruit_variable":"[job_profile]",
+        "variable_type": "user"
+    },
+    {
+        "name": "#joining_date",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+        "name": "#logo",
+        "value": "<p><img src='https://res.cloudinary.com/dp0y84e66/image/upload/v1568791251/logo.e5be347d_yf4q90.png' width='170' height='50'/></p>",
+        "variable_type": "system"
+    },
+    {
+        "name": "#salary",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+        "name": "#map_location",
+        "value": "https://www.google.co.in/maps/place/Excellence+Technologies/@28.5959817,77.3260043,17z/data=!3m1!4b1!4m5!3m4!1s0x390ce4fc8c75d9e:0x4ea29a8e67042fb9!8m2!3d28.595977!4d77.328193?hl=en",
+        "variable_type": "system"
+    },
+    {
+        "name": "#employee_user_name",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+        "name": "#training_completion_date",
+        "value": None,
+        "variable_type": "system"
+    },
+    {
+        "name": "#termination_date",
+        "value": None,
+        "variable_type": "system"
+    },
+    {
+        "name": "#designation",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+        "name": "#director_signature",
+        "value": "<p><strong>Excellence Technologies,</strong></p>\n<p><strong>Manish Prakash</strong></p>\n<p><strong>(Director)</strong></p>",
+        "variable_type": "user"
+    },
+    {
+        "name": "#venue",
+        "value": None,
+        "recruit_variable":"[venue]",
+        "variable_type": "system"
+    },
+    {
+        "name": "#company",
+        "value": None,
+        "recruit_variable":"[company]",
+        "variable_type": "system"
+    },
+    {
+        "name": "#hr_signature",
+        "value": None,
+        "recruit_variable":"[hr_signature]",
+        "variable_type": "system"
+    },
+    {
+        "name": "#numberOfDays",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+        "name": "#name",
+        "value": None,
+        "recruit_variable":"[name]",
+        "variable_type": "user"
+    },
+    {
+        
+        "name": "#fromDate",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+        
+        "name": "#toDate",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+       
+        "name": "#reason",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+        
+        "name": "#month",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+        
+        "name": "#details",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+        
+        "name": "#job_title",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+        
+        "name": "#login_id",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+       
+        "name": "#employee_id",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+        
+        "name": "#date_of_sending",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+        
+        "name": "#resignation_date",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+        "name": "#relieving_date",
+        "value": None,
+        "variable_type": "user"
+    },
+    { 
+        "name": "#lower_limit_salary",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+        "name": "#upper_limit_salary",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+        "name": "#increament_after_month",
+        "value": None,
+        "variable_type": "user"
+    },
+    {
+        "name": "#office_time",
+        "value": None,
+        "variable_type": "user"
+    }
+]
